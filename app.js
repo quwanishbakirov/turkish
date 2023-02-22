e76f0dff8bd02565e822496baf6f65f6c89202c1
quwanishbakirov <quwanishbakirov@gmail.com>
2023-02-23 00:05:35 +0500

    data has been added

--- app.js
+++ app.js
@@ -1,209 +1,28 @@
-let definition = [
-  "MEVSİMLER",
-  "İlkbahar",
-  "Yaz",
-  "Sonbahar",
-  "Kış",
-  "AYLAR",
-  "Ocak",
-  "Şubat",
-  "Mart",
-  "Nisan",
-  "Mayıs",
-  "Haziran",
-  "Temmuz",
-  "Ağustos",
-  "Eylül",
-  "Ekim",
-  "Kasım",
-  "Aralık",
-  "HAFTA GÜNLERİ",
-  "Pazartesi",
-  "Sali",
-  "Çarşamba",
-  "Perşembe",
-  "Cuma",
-  "Cumartesi",
-  "Pazar",
-  "ajanda",
-  "akıl",
-  "anma",
-  "ateş",
-  "balayı",
-  "biricik",
-  "birlikte",
-  "birliktelik",
-  "boya",
-  "buçuk",
-  "cimri",
-  "çeşitli",
-  "çeyrek",
-  "çorba",
-  "çoşku",
-  "dakika",
-  "dilek",
-  "dini",
-  "dişçi",
-  "dolunay",
-  "düğüm",
-  "egemenlik",
-  "evlilik",
-  "galeri",
-  "gelecek",
-  "gençlik",
-  "geveze",
-  "harf",
-  "hazırlık",
-  "hece",
-  "heyecan",
-  "kaza",
-  "kurum",
-  "kutlama",
-  "kültür",
-  "marş",
-  "mavi",
-  "mezuniyet",
-  "milli",
-  "mola",
-  "mor",
-  "nice",
-  "nikah",
-  "odun",
-  "ömür",
-  "palto",
-  "program",
-  "randevu",
-  "resmi",
-  "rezervasyon",
-  "saniye",
-  "sarı",
-  "saygıdeğer",
-  "sevgili",
-  "sürpriz",
-  "toplantı",
-  "ulusal",
-  "vakit",
-  "yeşil",
-  "yılbaşı",
-  "yıldönümü",
-  "zafer",
-  "zorluk"
-];
-
-let words = [
-  "Máwsimler",
-  "Báhár",
-  "Jaz",
-  "Gúz",
-  "Qıs",
-  "AYLAR",
-  "Yanvar",
-  "Fevral",
-  "Mart",
-  "Aprel",
-  "May",
-  "İyun",
-  "İyul",
-  "Avgust",
-  "Sentyabr",
-  "Oktyabr",
-  "Noyabr",
-  "Dekabr",
-  "HÁPTE KÚNLERI",
-  "Dúyshembi",
-  "Siyshembi",
-  "Sárshembi",
-  "Piyshembi",
-  "Juma",
-  "Shembi",
-  "Ekshembi",
-  "Kún tártibi",
-  "aqıl" ,
-  "eskertip ótiw",
-  "órt (gúlxan)",
-  "asal oyı",
-  "tek",
-  "birge",
-  "awızbirshilik",
-  "boyaw",
-  "yarımı",
-  "ashkóz",
-  "hár túrlı", 
-  "sherek",
-  "sorpa",
-  "qızıǵıwshılıq",
-  "minut",
-  "tilek",
-  "diniy",
-  "stomatolog",
-  "to'linoy",
-  "halqa" ,
-  "suverenliitet",
-  "neke",
-  "galereya" ,
-  "keleshek",
-  "jaslar" ,
-  "sáwbetlesiw",
-  "hárip",
-  "tayarlaw",
-  "buwın" ,
-  "tınıshsızlanıw" ,
-  "baxtsız hádiyse",
-  "shólkem",
-  "bayram" ,
-  "mádeniyat" ,
-  "gimn",
-  "kók",
-  "Pitkeriw",
-  "Milliy",
-  "tánepis",
-  "sıyarang",
-  "jaqsı",
-  "toy",
-  "aǵash" ,
-  "múddet",
-  "palto",
-  "programma" ,
-  "ushırasıw" ,
-  "rásmiy",
-  "bron qılıw" ,
-  "sekund",
-  "sarı" ,
-  "húrmetli",
-  "muhabbatım",
-  "syurpriz",
-  "jiynalis" ,
-  "Milliy",
-  "waqıt",
-  "jasıl" ,
-  "jańa Jıl" ,
-  "yubiley",
-  "jeńis",
-  "qıyınshılıq" 
-
-];
-
-
-
-let currentIndex = -1;
-
-function nextWord() {
-  let newIndex = currentIndex;
-  while (newIndex === currentIndex) {
-    newIndex = Math.floor(Math.random() * words.length);
+async function asyncReadFile(filename) {
+  try {
+    const response = await fetch(filename);
+    const contents = await response.text();
+    const words = contents.split(/\r?\n/);
+    return words;
+  } catch (err) {
+    console.log(err);
   }
+}
+
+async function showWord() {
+  const words = await asyncReadFile('./word.txt');
+  const defs = await asyncReadFile('./tran.txt');
   
+  const len = words.length;
+  const randomIndex = Math.floor(Math.random() * len);
+
+  const word = words[randomIndex];
+  const def = defs[randomIndex];
 
   var checkBox = document.getElementById("myCheck");
 
-  if (checkBox.checked == true){
-    currentIndex = newIndex;
-  
-    let word = words[currentIndex];
-    let def = definition[currentIndex];
-    document.getElementById('wordt').innerText = word;
-    document.getElementById('trans').innerText = def;
+  if (checkBox.checked == false){      
+  document.getElementById('word').innerHTML = word;
+  document.getElementById('def').innerHTML = def;
   }
-
-
 }
